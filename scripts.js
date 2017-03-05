a2a227cb5741e95326a6cab07d0005e75c45d262
Meena Vempaty <meena@through-the-floor.com>
2017-03-05 13:36:08 -0800

    Working on character dictionary

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+characters = {
+  "trump-kun": {
+  	"profile": "pepe.jpg",
+  	"image": "pepe.jpg"
+  },
+  "putin-senpai": {
+  	"profile": "pepe.jpg",
+  	"image": "pepe.jpg"
+  }
+};
 
+$( document ).ready(function() {
+    // Get canvas
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
+    // Draw basic image
     var img = new Image();
     img.src = "sketches.png";
     img.onload = function () {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     }
+
+    // Load characters into character lists
+    for (var character in characters) {
+      //TODO add entry into each list, with id as list selector
+      //alert(characters[character]["profile"]);
+    }
+
 });
